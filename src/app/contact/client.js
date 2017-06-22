bf2e766fb157a14359639f2530751c549d03827b
Siphokazi Manana <siphokazi.manana@advance.io>
2017-06-22 10:25:20 +0200

    new files added

---
+++ src/app/contact/client.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * Created by siphokazi on 2017/05/19.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+var Client = (function () {
+    //    model = new ClientQuery(1,"sfiso shabangu","sifisos@gmail.com", false, "hey there sipho");
+    /*constructor( id_: number, name : string, email: string, contact : boolean, query : string){
+        this.id = id_;
+        this.client_name = name;
+        this.client_email = email;
+        this.can_contact = contact;
+        this.client_query = query;
+    }*/
+    function Client() {
+    }
+    return Client;
+}());
+exports.Client = Client;
+//# sourceMappingURL=client.js.map
---
+++ src/app/contact/client.ts
@@ -0,0 +1,30 @@
+/**
+ * Created by siphokazi on 2017/05/19.
+ */
+
+
+export class Client{
+
+    public id: number;
+    public client_name: string;
+    public client_email: string;
+    public can_contact: boolean;
+    public client_query?: string;
+
+    //    model = new ClientQuery(1,"sfiso shabangu","sifisos@gmail.com", false, "hey there sipho");
+
+    /*constructor( id_: number, name : string, email: string, contact : boolean, query : string){
+        this.id = id_;
+        this.client_name = name;
+        this.client_email = email;
+        this.can_contact = contact;
+        this.client_query = query;
+    }*/
+    
+    constructor(){}
+
+
+
+
+
+}
